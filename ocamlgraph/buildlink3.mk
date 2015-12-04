cd7eb792db863c81a3e5b06591d720d15a0af2cd
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-04 18:04:28 -0500

    Add a package for OCamlgraph.

---
+++ ocamlgraph/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	ocamlgraph
+
+.if !defined(OCAMLGRAPH_BUILDLINK3_MK)
+OCAMLGRAPH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.ocamlgraph+=	ocamlgraph>=1.8.6
+BUILDLINK_PKGSRCDIR.ocamlgraph?=	../../eliteraspberries/ocamlgraph
+
+.include "../../lang/ocaml/buildlink3.mk"
+.include "../../devel/ocaml-findlib/buildlink3.mk"
+.endif	# OCAMLGRAPH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-ocamlgraph
