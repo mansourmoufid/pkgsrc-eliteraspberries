9f281891c8a5c34aa86bea212a59d47a89fcc27f
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-04 18:11:47 -0500

    Add a package for Alt-Ergo.

---
+++ alt-ergo/buildlink3.mk
@@ -0,0 +1,16 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	alt-ergo
+
+.if !defined(ALT_ERGO_BUILDLINK3_MK)
+ALT_ERGO_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.alt-ergo+=	alt-ergo>=0.99.1
+BUILDLINK_PKGSRCDIR.alt-ergo?=	../../eliteraspberries/alt-ergo
+
+.include "../../lang/ocaml/buildlink3.mk"
+.include "../../eliteraspberries/zarith/buildlink3.mk"
+.include "../../eliteraspberries/ocamlgraph/buildlink3.mk"
+.endif	# ALT_ERGO_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-alt-ergo
