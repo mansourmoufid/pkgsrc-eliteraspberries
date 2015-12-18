7b651fded9591b516053ee66d10268f8fa7e436d
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-18 13:44:46 -0500

    alt-ergo: Use eliteraspberries/ocaml.

--- alt-ergo/buildlink3.mk
+++ alt-ergo/buildlink3.mk
@@ -8,7 +8,7 @@ ALT_ERGO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.alt-ergo+=	alt-ergo>=0.99.1
 BUILDLINK_PKGSRCDIR.alt-ergo?=	../../eliteraspberries/alt-ergo
 
-.include "../../lang/ocaml/buildlink3.mk"
+.include "../../eliteraspberries/ocaml/buildlink3.mk"
 .include "../../eliteraspberries/zarith/buildlink3.mk"
 .include "../../eliteraspberries/ocamlgraph/buildlink3.mk"
 .endif	# ALT_ERGO_BUILDLINK3_MK
