e2cea0441f8cb8ac1d8c76f76dd6ee1a039b2683
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-21 17:00:41 -0500

    ocamlgraph: Use eliteraspberries/ocaml-findlib.

--- ocamlgraph/buildlink3.mk
+++ ocamlgraph/buildlink3.mk
@@ -5,11 +5,11 @@ BUILDLINK_TREE+=	ocamlgraph
 .if !defined(OCAMLGRAPH_BUILDLINK3_MK)
 OCAMLGRAPH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocamlgraph+=	ocamlgraph>=1.8.6
+BUILDLINK_API_DEPENDS.ocamlgraph+=	ocamlgraph>=1.8.6nb4
 BUILDLINK_PKGSRCDIR.ocamlgraph?=	../../eliteraspberries/ocamlgraph
 
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
-.include "../../devel/ocaml-findlib/buildlink3.mk"
+.include "../../eliteraspberries/ocaml-findlib/buildlink3.mk"
 .endif	# OCAMLGRAPH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-ocamlgraph
