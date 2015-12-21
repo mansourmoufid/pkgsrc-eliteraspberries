971f7c876353ae8d7c81e4e865e09bb0df400eb5
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-21 16:57:55 -0500

    camlp4: Use eliteraspberries/ocaml-findlib.

--- camlp4/buildlink3.mk
+++ camlp4/buildlink3.mk
@@ -5,11 +5,11 @@ BUILDLINK_TREE+=	camlp4
 .if !defined(CAMLP4_BUILDLINK3_MK)
 CAMLP4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.02+6nb2
+BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.02+6nb4
 BUILDLINK_PKGSRCDIR.camlp4?=	../../eliteraspberries/camlp4
 
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
-.include "../../devel/ocaml-findlib/buildlink3.mk"
+.include "../../eliteraspberries/ocaml-findlib/buildlink3.mk"
 .endif	# CAMLP4_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-camlp4
