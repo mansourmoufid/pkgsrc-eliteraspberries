1bd8b0337b89a12bf52c8c99b063f5190af1a4ed
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-21 17:03:04 -0500

    parmap: Use eliteraspberries/ocaml-findlib.

--- parmap/buildlink3.mk
+++ parmap/buildlink3.mk
@@ -5,11 +5,11 @@ BUILDLINK_TREE+=	parmap
 .if !defined(PARMAP_BUILDLINK3_MK)
 PARMAP_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.parmap+=	parmap>=1.0+rc7
+BUILDLINK_API_DEPENDS.parmap+=	parmap>=1.0+rc7nb3
 BUILDLINK_PKGSRCDIR.parmap?=	../../eliteraspberries/parmap
 
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
-.include "../../devel/ocaml-findlib/buildlink3.mk"
+.include "../../eliteraspberries/ocaml-findlib/buildlink3.mk"
 .endif	# PARMAP_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-parmap
