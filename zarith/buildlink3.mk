73e77b0b1b41ef49c0b52f4bfa87862b3d00ac18
Mansour Moufid <mansourmoufid@gmail.com>
2015-12-21 17:01:53 -0500

    zarith: Use eliteraspberries/ocaml-findlib.

--- zarith/buildlink3.mk
+++ zarith/buildlink3.mk
@@ -5,12 +5,12 @@ BUILDLINK_TREE+=	zarith
 .if !defined(ZARITH_BUILDLINK3_MK)
 ZARITH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.zarith+=	zarith>=1.4.1
+BUILDLINK_API_DEPENDS.zarith+=	zarith>=1.4.1nb3
 BUILDLINK_PKGSRCDIR.zarith?=	../../eliteraspberries/zarith
 
 .include "../../devel/gmp/buildlink3.mk"
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
-.include "../../devel/ocaml-findlib/buildlink3.mk"
+.include "../../eliteraspberries/ocaml-findlib/buildlink3.mk"
 .endif	# ZARITH_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-zarith
