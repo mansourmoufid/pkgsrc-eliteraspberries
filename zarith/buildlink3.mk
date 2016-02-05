8073e7a28c800d8ee84b7e41ae7f996ffd43f58c
Mansour Moufid <mansourmoufid@gmail.com>
2016-02-04 19:30:10 -0500

    zarith: Use eliteraspberries/gmp.

--- zarith/buildlink3.mk
+++ zarith/buildlink3.mk
@@ -5,10 +5,10 @@ BUILDLINK_TREE+=	zarith
 .if !defined(ZARITH_BUILDLINK3_MK)
 ZARITH_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.zarith+=	zarith>=1.4.1nb3
+BUILDLINK_API_DEPENDS.zarith+=	zarith>=1.4.1nb4
 BUILDLINK_PKGSRCDIR.zarith?=	../../eliteraspberries/zarith
 
-.include "../../devel/gmp/buildlink3.mk"
+.include "../../eliteraspberries/gmp/buildlink3.mk"
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
 .include "../../eliteraspberries/ocaml-findlib/buildlink3.mk"
 .endif	# ZARITH_BUILDLINK3_MK
