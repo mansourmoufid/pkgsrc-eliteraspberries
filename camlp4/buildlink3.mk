14bf3450822c773849f162dfc4adf668f8a2f2d5
Mansour Moufid <mansourmoufid@gmail.com>
2016-02-29 13:12:20 -0500

    camlp4: Fix version number.

--- camlp4/buildlink3.mk
+++ camlp4/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	camlp4
 .if !defined(CAMLP4_BUILDLINK3_MK)
 CAMLP4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.02+6nb4
+BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.02.6nb5
 BUILDLINK_PKGSRCDIR.camlp4?=	../../eliteraspberries/camlp4
 
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
