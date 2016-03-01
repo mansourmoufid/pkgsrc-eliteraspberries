1bda7f85e4c26d6528868a19f20b7c729f0bd62a
Mansour Moufid <mansourmoufid@gmail.com>
2016-02-29 20:20:23 -0500

    ocaml-findlib: Build native code.

--- ocaml-findlib/buildlink3.mk
+++ ocaml-findlib/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	ocaml-findlib
 .if !defined(OCAML_FINDLIB_BUILDLINK3_MK)
 OCAML_FINDLIB_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ocaml-findlib+=	ocaml-findlib>=1.6.1nb3
+BUILDLINK_API_DEPENDS.ocaml-findlib+=	ocaml-findlib>=1.6.1nb4
 BUILDLINK_PKGSRCDIR.ocaml-findlib?=	../../eliteraspberries/ocaml-findlib
 
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
