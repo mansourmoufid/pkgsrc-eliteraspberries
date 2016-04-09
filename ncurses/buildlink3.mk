8c0b9cbce016710c28c9d7d137f54984d508b24f
Mansour Moufid <mansourmoufid@gmail.com>
2016-04-09 10:59:19 -0400

    ncurses: Set the BUILDLINK_INCDIRS variable.

--- ncurses/buildlink3.mk
+++ ncurses/buildlink3.mk
@@ -6,8 +6,10 @@ BUILDLINK_TREE+=	ncurses
 .if !defined(NCURSES_BUILDLINK3_MK)
 NCURSES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ncurses+=	ncurses>=6.0nb1
+BUILDLINK_API_DEPENDS.ncurses+=	ncurses>=6.0nb3
 BUILDLINK_PKGSRCDIR.ncurses?=	../../eliteraspberries/ncurses
 .endif	# NCURSES_BUILDLINK3_MK
+
+BUILDLINK_INCDIRS.ncurses+=	include/ncurses
 
 BUILDLINK_TREE+=	-ncurses
