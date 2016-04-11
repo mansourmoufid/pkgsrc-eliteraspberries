39f1068f5760bf8442afc173327454e6ecda634b
Mansour Moufid <mansourmoufid@gmail.com>
2016-04-11 14:49:02 -0400

    eliteraspberries/alt-ergo: Specify build dependencies.

--- alt-ergo/buildlink3.mk
+++ alt-ergo/buildlink3.mk
@@ -8,6 +8,9 @@ ALT_ERGO_BUILDLINK3_MK:=
 BUILDLINK_API_DEPENDS.alt-ergo+=	alt-ergo>=1.01nb2
 BUILDLINK_PKGSRCDIR.alt-ergo?=	../../eliteraspberries/alt-ergo
 
+BUILDLINK_DEPMETHOD.ocaml=	build
+BUILDLINK_DEPMETHOD.ocamlgraph=	build
+BUILDLINK_DEPMETHOD.zarith=	build
 .include "../../eliteraspberries/gmp/buildlink3.mk"
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
 .include "../../eliteraspberries/ocamlgraph/buildlink3.mk"
