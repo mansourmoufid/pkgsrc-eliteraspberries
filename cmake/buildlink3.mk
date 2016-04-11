f271c74508fa4e821b1a5288c9df93ad7ea420d6
Mansour Moufid <mansourmoufid@gmail.com>
2016-04-11 11:43:11 -0400

    cmake: Add a buildlink3.mk file.

---
+++ cmake/buildlink3.mk
@@ -0,0 +1,20 @@
+# $NetBSD$
+# BUILDLINK_DEPMETHOD.cmake?=	build
+
+BUILDLINK_TREE+=	cmake
+
+.if !defined(CMAKE_BUILDLINK3_MK)
+CMAKE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.cmake+=	cmake>=3.5.1
+BUILDLINK_PKGSRCDIR.cmake?=	../../eliteraspberries/cmake
+
+.include "../../archivers/bzip2/buildlink3.mk"
+.include "../../devel/zlib/buildlink3.mk"
+.include "../../eliteraspberries/curl/buildlink3.mk"
+.include "../../eliteraspberries/libarchive/buildlink3.mk"
+.include "../../eliteraspberries/ncurses/buildlink3.mk"
+.include "../../textproc/expat/buildlink3.mk"
+.endif	# CMAKE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-cmake
