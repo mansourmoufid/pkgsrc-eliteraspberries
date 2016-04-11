15f663f142e3d59b96c5548e1100fec5f510f4b3
Mansour Moufid <mansourmoufid@gmail.com>
2016-04-11 14:35:02 -0400

    eliteraspberries/alt-ergo: Add a dependency.

--- alt-ergo/buildlink3.mk
+++ alt-ergo/buildlink3.mk
@@ -5,12 +5,13 @@ BUILDLINK_TREE+=	alt-ergo
 .if !defined(ALT_ERGO_BUILDLINK3_MK)
 ALT_ERGO_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.alt-ergo+=	alt-ergo>=0.99.1
+BUILDLINK_API_DEPENDS.alt-ergo+=	alt-ergo>=1.01nb2
 BUILDLINK_PKGSRCDIR.alt-ergo?=	../../eliteraspberries/alt-ergo
 
+.include "../../eliteraspberries/gmp/buildlink3.mk"
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
-.include "../../eliteraspberries/zarith/buildlink3.mk"
 .include "../../eliteraspberries/ocamlgraph/buildlink3.mk"
+.include "../../eliteraspberries/zarith/buildlink3.mk"
 .endif	# ALT_ERGO_BUILDLINK3_MK
 
 BUILDLINK_TREE+=	-alt-ergo
