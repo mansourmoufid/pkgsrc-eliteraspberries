15af6fab8d68be512a942ac687c50ada63cd20ca
Mansour Moufid <mansourmoufid@gmail.com>
2016-04-15 08:49:14 -0400

    Add a package for camlp5.

---
+++ camlp5/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+# BUILDLINK_DEPMETHOD.camlp5?=	build
+
+BUILDLINK_TREE+=	camlp5
+
+.if !defined(CAMLP5_BUILDLINK3_MK)
+CAMLP5_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=6.15
+BUILDLINK_PKGSRCDIR.camlp5?=	../../eliteraspberries/camlp5
+
+.include "../../eliteraspberries/ocaml/buildlink3.mk"
+.endif	# CAMLP5_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-camlp5
