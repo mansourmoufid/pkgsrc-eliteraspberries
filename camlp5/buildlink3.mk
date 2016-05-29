59d57277052e695e9e015564a8c6a5e3eedb5cdf
Mansour Moufid <mansourmoufid@gmail.com>
2016-05-29 18:27:25 -0400

    camlp5: Bump to version 6.16.

--- camlp5/buildlink3.mk
+++ camlp5/buildlink3.mk
@@ -6,7 +6,7 @@ BUILDLINK_TREE+=	camlp5
 .if !defined(CAMLP5_BUILDLINK3_MK)
 CAMLP5_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=6.15
+BUILDLINK_API_DEPENDS.camlp5+=	camlp5>=6.16
 BUILDLINK_PKGSRCDIR.camlp5?=	../../eliteraspberries/camlp5
 
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
