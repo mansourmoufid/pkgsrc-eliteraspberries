891a7ce2dc3091c47d6c329963477961af93a4a3
Mansour Moufid <mansourmoufid@gmail.com>
2016-05-29 18:23:15 -0400

    camlp4: Bump to version 4.02.7.

--- camlp4/buildlink3.mk
+++ camlp4/buildlink3.mk
@@ -1,11 +1,12 @@
 # $NetBSD$
+# BUILDLINK_DEPMETHOD.camlp4?=	build
 
 BUILDLINK_TREE+=	camlp4
 
 .if !defined(CAMLP4_BUILDLINK3_MK)
 CAMLP4_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.02.6nb5
+BUILDLINK_API_DEPENDS.camlp4+=	camlp4>=4.02.7
 BUILDLINK_PKGSRCDIR.camlp4?=	../../eliteraspberries/camlp4
 
 .include "../../eliteraspberries/ocaml/buildlink3.mk"
