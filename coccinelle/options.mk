36866910ddecf5aa00a4154ab8e0bedf2856a4e9
Mansour Moufid <mansourmoufid@gmail.com>
2016-11-13 15:09:14 -0500

    eliteraspberries/coccinelle: Add Python scripting support.

--- coccinelle/options.mk
+++ coccinelle/options.mk
@@ -1,6 +1,7 @@
 PKG_OPTIONS_VAR=	PKG_OPTIONS.coccinelle
-PKG_SUPPORTED_OPTIONS=	pcre
+PKG_SUPPORTED_OPTIONS=	pcre python
 PKG_SUGGESTED_OPTIONS=	pcre
+PLIST_VARS+=		python
 
 .include "../../mk/bsd.options.mk"
 
@@ -12,4 +13,12 @@ CONFIGURE_ARGS+=	--enable-pcre-syntax
 .else
 CONFIGURE_ARGS+=	--disable-pcre
 CONFIGURE_ARGS+=	--disable-pcre-syntax
+.endif
+
+.if !empty(PKG_OPTIONS:Mpython)
+CONFIGURE_ARGS+=	--enable-python
+PLIST.python=		yes
+.include "../../eliteraspberries/python/buildlink3.mk"
+.else
+CONFIGURE_ARGS+=	--disable-python
 .endif
