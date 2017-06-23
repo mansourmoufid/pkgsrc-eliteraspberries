93003881ca8aea3578c876ef9412492b5ef4ee33
Mansour Moufid <mansourmoufid@gmail.com>
2017-06-23 13:45:16 -0400

    eliteraspberries/python-numpy: Define NPY_USE_PYMEM to 0.

--- python-numpy/patches/patch-numpy_core_include_numpy_ndarraytypes.h
+++ python-numpy/patches/patch-numpy_core_include_numpy_ndarraytypes.h
@@ -1,7 +1,16 @@
 $NetBSD$
 
---- numpy/core/include/numpy/ndarraytypes.h.orig	2017-06-17 14:20:23.000000000 -0400
-+++ numpy/core/include/numpy/ndarraytypes.h	2017-06-17 14:21:02.000000000 -0400
+--- numpy/core/include/numpy/ndarraytypes.h.orig	2016-03-27 11:25:31.000000000 -0400
++++ numpy/core/include/numpy/ndarraytypes.h	2017-06-20 14:15:24.000000000 -0400
+@@ -325,7 +325,7 @@
+ 
+   /* Data buffer - PyDataMem_NEW/FREE/RENEW are in multiarraymodule.c */
+ 
+-#define NPY_USE_PYMEM 1
++#define NPY_USE_PYMEM 0
+ 
+ #if NPY_USE_PYMEM == 1
+ #define PyArray_malloc PyMem_Malloc
 @@ -640,9 +640,9 @@
  typedef struct tagPyArrayObject_fields {
      PyObject_HEAD
