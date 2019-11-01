fdb72c1a9eb695ba995712eab067db9741cce078
Mansour Moufid <mansourmoufid@gmail.com>
2019-10-31 23:56:57 -0400

    eliteraspberries/ncurses: Enable wide character support.

--- ncurses/buildlink3.mk
+++ ncurses/buildlink3.mk
@@ -1,15 +1,14 @@
 # $NetBSD$
-# BUILDLINK_DEPMETHOD.ncurses?=	build
 
 BUILDLINK_TREE+=	ncurses
 
 .if !defined(NCURSES_BUILDLINK3_MK)
 NCURSES_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.ncurses+=	ncurses>=6.0nb3
+BUILDLINK_API_DEPENDS.ncurses+=	ncurses>=6.1
 BUILDLINK_PKGSRCDIR.ncurses?=	../../eliteraspberries/ncurses
 .endif	# NCURSES_BUILDLINK3_MK
 
-BUILDLINK_INCDIRS.ncurses+=	include/ncurses
+BUILDLINK_INCDIRS.ncurses+=		include/ncursesw
 
 BUILDLINK_TREE+=	-ncurses
