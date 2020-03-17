286e687a600f538e38b5c3c51698a587ac75c3a3
Mansour Moufid <mansourmoufid@gmail.com>
2020-03-17 16:02:11 -0400

    eliteraspberries/pythonxy: Do not overwrite files.

--- pythonxy/build.sh
+++ pythonxy/build.sh
@@ -7,8 +7,9 @@ for V in 2.7.16 3.5.7; do
     Y="$(echo ${V} | awk -F. '{print $2}')"
     Z="$(echo ${V} | awk -F. '{print $3}')"
     mkdir -p ../../eliteraspberries/python${X}${Y}
-    cp DESCR ../../eliteraspberries/python${X}${Y}/
-    cp PLIST ../../eliteraspberries/python${X}${Y}/
+    for f in DESCR PLIST; do
+        test -f ../python${X}${Y}/${f} || cp ${f} ../python${X}${Y}/
+    done
     for F in Makefile buildlink3.mk fix-suffixes.sh; do
         sed -e "s/{{X}}/${X}/g" \
             -e "s/{{Y}}/${Y}/g" \
