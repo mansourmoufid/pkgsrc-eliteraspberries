f6e9db6264d0fe5a71ac9579d618972983cb583e
Mansour Moufid <mansourmoufid@gmail.com>
2021-08-04 18:57:51 -0400

    eliteraspberries/cmake: Set run-time search paths.
    Build and install only specified targets.

--- cmake/cmake.mk
+++ cmake/cmake.mk
@@ -13,8 +13,8 @@ CMAKE_ARGS+=	-DCMAKE_C_FLAGS="${CFLAGS}"
 CMAKE_ARGS+=	-DCMAKE_CXX_FLAGS="${CXXFLAGS}"
 CMAKE_ARGS+=	-DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}"
 CMAKE_ARGS+=	-DCMAKE_INSTALL_PREFIX="${PREFIX}"
-CMAKE_ARGS+=	-DCMAKE_BUILD_RPATH="${PREFIX}"
-# CMAKE_ARGS+=	-DCMAKE_INSTALL_RPATH="${PREFIX}"
+CMAKE_ARGS+=	-DCMAKE_BUILD_RPATH="${BUILDLINK_DIR}"
+CMAKE_ARGS+=	-DCMAKE_INSTALL_RPATH="${PREFIX}"
 
 CMAKE_DIR=			build
 
@@ -24,8 +24,18 @@ do-configure:
 
 BUILD_DIRS=			${CMAKE_DIR}
 
+do-build:
+	cd ${WRKSRC}/${CMAKE_DIR} && \
+	for x in ${BUILD_TARGET}; do \
+		env ${MAKE_ENV} DESTDIR=${DESTDIR} \
+			cmake --build . --target $${x}; \
+	done
+
 do-install:
 	cd ${WRKSRC}/${CMAKE_DIR} && \
-	env ${MAKE_ENV} DESTDIR=${DESTDIR} cmake --install .
+	for x in ${INSTALL_TARGET}; do \
+		env ${MAKE_ENV} DESTDIR=${DESTDIR} \
+			cmake --build . --target $${x}; \
+	done
 
 .endif # CMAKE_MK
