916e5df358cfbbbab26d4fb80c21942613522b18
Mansour Moufid <mansourmoufid@gmail.com>
2021-11-17 22:29:31 -0500

    eliteraspberries/python-setuptools: Upgrade to version 49.6.0.

--- python-setuptools/patches/patch-pkg_resources___init__.py
+++ python-setuptools/patches/patch-pkg_resources___init__.py
@@ -1,8 +1,8 @@
 $NetBSD$
 
---- pkg_resources/__init__.py.orig	2020-01-24 10:41:22.000000000 -0500
-+++ pkg_resources/__init__.py	2020-01-24 10:41:41.000000000 -0500
-@@ -2641,9 +2641,8 @@
+--- pkg_resources/__init__.py.orig	2020-08-14 02:12:36.000000000 -0400
++++ pkg_resources/__init__.py	2021-11-17 17:53:58.000000000 -0500
+@@ -2806,9 +2807,8 @@
  
      def egg_name(self):
          """Return what this distribution's standard .egg filename should be"""
--- python/setuppy.mk
+++ python/setuppy.mk
@@ -8,21 +8,14 @@ SETUPPY_INSTALL_ARGS+=	--no-compile
 SETUPPY_INSTALL_ARGS+=	--prefix=${DESTDIR}${PREFIX}
 SETUPPY_INSTALL_ARGS+=	--record=PLIST.in
 
-#SETUPPY_INSTALL_ARGS+=	--single-version-externally-managed
-
 do-build:
 	cd ${WRKSRC} && \
 	env ${MAKE_ENV} \
-	python setup.py ${SETUPPY_ARGS} \
-		build_ext ${SETUPPY_BUILDEXT_ARGS} \
-		build ${SETUPPY_BUILD_ARGS}
+	python setup.py ${SETUPPY_ARGS} build ${SETUPPY_BUILD_ARGS}
 
 do-install:
 	cd ${WRKSRC} && \
 	env ${MAKE_ENV} \
-	python setup.py ${SETUPPY_ARGS} \
-		build_ext ${SETUPPY_BUILDEXT_ARGS} \
-		build ${SETUPPY_BUILD_ARGS} \
-		install ${SETUPPY_INSTALL_ARGS}
+	python setup.py ${SETUPPY_ARGS} install ${SETUPPY_INSTALL_ARGS}
 
 .endif	# PYTHON_SETUPPY_MK
