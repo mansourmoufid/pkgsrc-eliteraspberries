a675dfb1ae6bd9fde1a501104a37b398c5012316
Mansour Moufid <mansourmoufid@gmail.com>
2022-11-02 16:02:16 -0400

    Add a package for the dispatch library framework of PyObjC.

---
+++ python-pyobjc-framework-libdispatch/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	python-pyobjc-framework-libdispatch
+
+.if !defined(PYTHON_PYOBJC_FRAMEWORK_LIBDISPATCH_BUILDLINK3_MK)
+PYTHON_PYOBJC_FRAMEWORK_LIBDISPATCH_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.python-pyobjc-framework-libdispatch+=	python-pyobjc-framework-libdispatch>=8.0
+BUILDLINK_PKGSRCDIR.python-pyobjc-framework-libdispatch?=	../../eliteraspberries/python-pyobjc-framework-libdispatch
+
+.include "../../eliteraspberries/python/buildlink3.mk"
+.include "../../eliteraspberries/python-setuptools/buildlink3.mk"
+.endif	# PYTHON_PYOBJC_FRAMEWORK_LIBDISPATCH_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-python-pyobjc-framework-libdispatch
