77111d955aa5c2022e4c3f98d6920ed6423da8ab
Mansour Moufid <mansourmoufid@gmail.com>
2025-08-19 15:39:50 -0400

    sdl2: Update to version 2.30.11.

--- sdl2/patches/patch-src_video_cocoa_SDL_cocoawindow.m
+++ sdl2/patches/patch-src_video_cocoa_SDL_cocoawindow.m
@@ -1,12 +1,10 @@
-$NetBSD$
-
---- src/video/cocoa/SDL_cocoawindow.m.orig	2018-09-12 23:58:48.000000000 -0400
-+++ src/video/cocoa/SDL_cocoawindow.m	2018-09-12 23:59:19.000000000 -0400
-@@ -22,6 +22,7 @@
- 
- #if SDL_VIDEO_DRIVER_COCOA
- 
+--- src/video/cocoa/SDL_cocoawindow.m.orig	2025-08-19 15:17:26
++++ src/video/cocoa/SDL_cocoawindow.m	2025-08-19 15:18:38
+@@ -18,6 +18,7 @@
+      misrepresented as being the original software.
+   3. This notice may not be removed or altered from any source distribution.
+ */
 +#include <AvailabilityMacros.h>
- #if MAC_OS_X_VERSION_MAX_ALLOWED < 1070
- # error SDL for Mac OS X must be built with a 10.7 SDK or above.
- #endif /* MAC_OS_X_VERSION_MAX_ALLOWED < 1070 */
+ #include "../../SDL_internal.h"
+ 
+ #ifdef SDL_VIDEO_DRIVER_COCOA
